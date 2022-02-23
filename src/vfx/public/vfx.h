b20d7d8a330d5da31888704128aa5f66286c3a0f
Jason <JasonBee@163.com>
2022-02-23 10:29:58 +0800

    vfx

--- src/vfx/public/vfx.h
+++ src/vfx/public/vfx.h
@@ -1,12 +1,12 @@
 //
-//  Vmg.h
+//  Vfx.h
 //  libVisualEffect
 //
 //  Created by Jason on 2022/2/23.
 //
 
-#ifndef Vmg_h
-#define Vmg_h
+#ifndef Vfx_h
+#define Vfx_h
 
 
-#endif /* Vmg_h */
+#endif /* Vfx_h */
